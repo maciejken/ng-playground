d9183fe905bf7075691500a4dab87cef9b86f895
Kedra <maciej.kedra@capgemini.com>
2018-07-29 23:06:10 +0200

    add dropdown menu (level4)

--- src/app/components/sidebar/menu.ts
+++ src/app/components/sidebar/menu.ts
@@ -1,10 +1,203 @@
 export const MENU = {
     level1: [
         {
-            name: 'Abc defghi jklm'
+            name: 'Latinorum',
+            level2: [
+                {
+                    name: 'Lorem ipsum dolor',
+                    level3: [
+                        {
+                            name: 'Lorem porem'
+                        },
+                        {
+                            name: 'Ipsum hipsum'
+                        },
+                        {
+                            name: 'Dolor color'
+                        }
+                    ]
+                },
+                {
+                    name: 'Nullam ut sem',
+                    level3: [
+                        {
+                            name: 'Nullam bullam'
+                        },
+                        {
+                            name: 'Seminullam'
+                        }
+                    ]
+                },
+                {
+                    name: 'Fusce tellus',
+                    level3: [
+                        {
+                            name: 'Fusce lintur'
+                        },
+                        {
+                            name: 'Lala lintur'
+                        },
+                        {
+                            name: 'Telerancum'
+                        }
+                    ]
+                },
+                {
+                    name: 'Proin aliquam dolor',
+                    level3: [
+                        {
+                            name: 'Promilum alcolum'
+                        },
+                        {
+                            name: 'Testorum dolorum'
+                        },
+                        {
+                            name: 'In vino veritas'
+                        }
+                    ]
+                },
+                {
+                    name: 'Nullam pharetra sit',
+                    level3: [
+                        {
+                            name: 'Nihil novi sub sole'
+                        },
+                        {
+                            name: 'Vanitas vanitatum'
+                        },
+                        {
+                            name: 'Et omnia vanitas'
+                        }
+                    ]
+                },
+            ]
         },
         {
-            name: 'Nopqr stuv xyz'
+            name: 'Piosenki dla dzieci i młodzieży',
+            level2: [
+                {
+                    name: 'Litwo! Ojczyzno moja!',
+                    level3: [
+                        {
+                            name: 'Wlazł kotek'
+                        },
+                        {
+                            name: 'Na płotek'
+                        },
+                        {
+                            name: 'I mruga'
+                        }
+                    ]
+                },
+                {
+                    name: 'Dunaj tam nie mógł',
+                    level3: [
+                        {
+                            name: 'Ładna to piosenka'
+                        },
+                        {
+                            name: 'Niedługa'
+                        }
+                    ]
+                },
+                {
+                    name: 'Bernardynie',
+                    level3: [
+                        {
+                            name: 'Niedługa, niekrótka'
+                        },
+                        {
+                            name: 'Lecz w sam raz'
+                        },
+                        {
+                            name: 'Zaśpiewaj koteczku'
+                        },
+                        {
+                            name: 'Jeszcze raz'
+                        }
+                    ]
+                },
+                {
+                    name: 'Wojskiego teże',
+                    level3: [
+                        {
+                            name: 'Mój dziadek dał mi psa',
+                            level4: [
+                                {
+                                    name: 'Bardzo go lubię'
+                                },
+                                {
+                                    name: 'Przyznać to muszę'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Pies cztery łapy ma',
+                            level4: [
+                                {
+                                    name: 'Puszek okruszek'
+                                },
+                                {
+                                    name: 'Kłębuszek'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Litwę murami żelaz',
+                    level3: [
+                        {
+                            name: 'Normalny mówią pies',
+                            level4: [
+                                {
+                                    name: 'Jest między nami sympatii nić'
+                                },
+                                {
+                                    name: 'Wystarczy tylko'
+                                },
+                                {
+                                    name: 'Że się poruszę'
+                                },
+                                {
+                                    name: 'Zaraz przybiega, by ze mną być'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'Nieprawda, coś w nim jest'
+                        },
+                        {
+                            name: 'Ma delikatny głos'
+                        }
+                        {
+                            name: 'I bardzo czujny nos'
+                        }
+                    ]
+                },
+                {
+                    name: 'Takim był, lecz lekki',
+                    level3: [
+                        {
+                            name: 'I kilka śmiesznych łat'
+                        },
+                        {
+                            name: 'Niewiele liczy lat'
+                        }
+                    ]
+                },
+                {
+                    name: 'Ty jesteś jak zdrowie',
+                    level3: [
+                        {
+                            name: 'Puszek okruszek'
+                        },
+                        {
+                            name: 'Puszek kłębuszek'
+                        }
+                    ]
+                }
+            ]
         }
     ]
 };
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,10 +9,28 @@ import { MENU } from './menu';
 export class SidebarComponent implements OnInit {
 
   menu = MENU;
+  level2;
 
   constructor() { }
 
   ngOnInit() {
+    this.resetFilter();
+  }
+
+  filterLevel2(index) {
+    this.level2 = this.menu.level1[index].level2;
+  }
+
+  resetFilter() {
+    let level2 = [];
+    this.menu.level1.map(item => {
+      level2 = level2.concat(item.level2);
+    });
+    setTimeout(() => this.level2 = level2, 150);
+  }
+
+  countWidth(str, fontSize = 24) {
+    return Math.round(str.length * 0.6 * fontSize);
   }
 
 }
