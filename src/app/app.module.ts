43d79751928f5a6809ad175867c520379bfc2914
Maciej Kędra <maciejken@gmail.com>
2018-12-17 22:10:25 +0100

    use mat icons inside inputs (matSuffix)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { DialogComponent } from './dialog/dialog.component';
@@ -25,6 +25,7 @@ import { FormFieldComponent } from './form-field/form-field.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
     BrowserAnimationsModule
   ],
   entryComponents: [
